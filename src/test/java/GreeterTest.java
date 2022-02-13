e2b0d2c4923e87a850b6533f44019a2f0a2187a9
Jlee108 <jbacked@yahoo.com>
2022-02-13 19:19:28 +0000

    Initial commit

---
+++ src/test/java/GreeterTest.java
@@ -0,0 +1,13 @@
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GreeterTest {
+
+    @Test
+    public void testThatGreeterReturnsTheCorrectGreeting() {
+        assertThat(new Greeter().getGreeting()).isEqualTo("Hello, World!");
+    }
+
+}
